ea6a475ff0ab7834db78d6d5ffb7a9518db641a4
Christina Koss <christina.koss@bccn-berlin.de>
2018-05-08 10:45:02 +0200

    have unzip as a function that takes the directory as argument

--- unzip_all.py
+++ unzip_all.py
@@ -1,13 +1,13 @@
 import os, zipfile
 
-dir_name = 'path'
-extension = ".zip"
+def unzip_folder(dir_name):
 
-for item in os.listdir(dir_name): # loop through items in dir
-    if item.endswith(extension): # check for ".zip" extension
-        file_name = dir_name + "/" + item
-        #file_name = os.path.abspath(item) # get full path of files
-        zip_ref = zipfile.ZipFile(file_name) # create zipfile object
-        zip_ref.extractall(dir_name) # extract file to dir
-        zip_ref.close() # close file
-        os.remove(file_name) # delete zipped file
+    extension = ".zip"
+    for item in os.listdir(dir_name): # loop through items in dir
+        if item.endswith(extension): # check for ".zip" extension
+            file_name = dir_name + "/" + item
+            #file_name = os.path.abspath(item) # get full path of files
+            zip_ref = zipfile.ZipFile(file_name) # create zipfile object
+            zip_ref.extractall(dir_name) # extract file to dir
+            zip_ref.close() # close file
+            os.remove(file_name) # delete zipped file
