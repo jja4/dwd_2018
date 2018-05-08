98eaa4725c1880bbb041bd9bb970217180f34ede
Christina Koss <christina.koss@bccn-berlin.de>
2018-05-08 10:50:55 +0200

    have useftp as a function that takes userpath as argument

--- useftp.py
+++ useftp.py
@@ -2,29 +2,29 @@ from ftplib import FTP
 import os
 
 server = "ftp-cdc.dwd.de"
-serverpath = "/pub/CDC/observations_germany/climate/hourly"
-userpath = "/Users/pcp/test"
 
-ftp = FTP(server)
-ftp.login()
+def download_data(userpath):
 
-# get historical data, if it isn't already stored
-histpath = os.path.join(userpath,'pub/CDC/observations_germany/climate/daily/kl/historical/')
-if not os.path.isdir(histpath):
-    filenames = ftp.nlst('pub/CDC//observations_germany/climate/daily/kl/historical')
-    os.makedirs(histpath)
+    ftp = FTP(server)
+    ftp.login()
+
+    # get historical data, if it isn't already stored
+    histpath = os.path.join(userpath,'pub/CDC/observations_germany/climate/daily/kl/historical/')
+    if not os.path.isdir(histpath):
+        filenames = ftp.nlst('pub/CDC//observations_germany/climate/daily/kl/historical')
+        os.makedirs(histpath)
+
+        for filename in filenames:
+            local_filename = os.path.join(userpath, filename)
+            file = open(local_filename, 'wb')
+            ftp.retrbinary('RETR '+ filename, file.write)
+            file.close()
+
+    filenames = ftp.nlst('pub/CDC//observations_germany/climate/daily/kl/recent')
+    os.makedirs(os.path.join(userpath,'pub/CDC/observations_germany/climate/daily/kl/recent/'))
 
     for filename in filenames:
         local_filename = os.path.join(userpath, filename)
         file = open(local_filename, 'wb')
         ftp.retrbinary('RETR '+ filename, file.write)
         file.close()
-
-filenames = ftp.nlst('pub/CDC//observations_germany/climate/daily/kl/recent')
-os.makedirs(os.path.join(userpath,'pub/CDC/observations_germany/climate/daily/kl/recent/'))
-
-for filename in filenames:
-    local_filename = os.path.join(userpath, filename)
-    file = open(local_filename, 'wb')
-    ftp.retrbinary('RETR '+ filename, file.write)
-    file.close()
