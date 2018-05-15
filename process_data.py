525333f10a5aecdac1b3464ef96638ba6f36f631
Nelson Niemeyer <nelson_niemeyer@hotmail.de>
2018-05-15 12:47:38 +0200

    separating into 3 functions

--- process_data.py
+++ process_data.py
@@ -4,6 +4,14 @@ import os
 import glob
 
 
+def process_data(userpath,stationnumber,time_interval):
+    #function for database group to call
+    if time_interval=='daily':
+        merged = merge_hisrec_daily(userpath,stationnumber)
+    elif time_interval=='hourly':
+        merged = merge_hisrec_hourly(userpath,stationnumber)
+    return clean_merged(merged)
+
 def merge_hisrec_daily(userpath,stationnumber):
     histpath = os.path.join(userpath, 'pub','CDC','observations_germany','climate','daily','kl','historical')
     recpath  = os.path.join(userpath, 'pub','CDC','observations_germany','climate','daily','kl','recent')
@@ -21,14 +29,7 @@ def merge_hisrec_daily(userpath,stationnumber):
     merged=pd.concat([histdata,recentdata])
     return merged
 
-def clean_merged_daily(merged):
+def clean_merged(merged):
     merged_clean = merged.replace(-999, np.nan, regex=True)
     merged_clean = merged_clean.drop(['eor'],axis=1)
-
-    merged_clean.columns =['Station_ID','Date','Quality_of_following2_columns',
-'Wind_speed_max','Wind_speed_average','Quality_of_following12_columns','rain',
- 'type_of_rain','sunny_hours','snow_height',
- 'Average fraction of sky covered by clouds','steam_pressure_average','Air_pressure_average',
- 'Air_temperature_average','relative_humidity_daily_average','Air_temp_max_2m','Air_temp_min_2m',
- 'Air_temp_5cm']
     return merged_clean
