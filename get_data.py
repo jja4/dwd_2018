fe58475b3cabb56fa60f1f16928e1a83c85ac070
Nikolai <nikolai.zaki@bccn-berlin.de>
2018-05-22 12:16:56 +0200

    added command line options for calling get_data

--- get_data.py
+++ get_data.py
@@ -2,11 +2,16 @@ import os
 import sys
 from useftp import download_data
 from unzip_all import unzip_folder
+import getopt
 
-def get_data(userpath,historical=True,recent=True,hourly=True,VERBOSE=True):
+def get_data(userpath, historical = True, recent = True, hourly = True, verbose = True):
     #download the files
     print("getting data")
-    download_data(userpath,historical,recent,hourly,VERBOSE)
+    download_data(userpath = userpath,
+                  historical = historical,
+                  recent = recent,
+                  hourly = hourly,
+                  verbose = verbose)
 
     #unzip the files
     print("unzipping data")
@@ -23,11 +28,53 @@ def get_data(userpath,historical=True,recent=True,hourly=True,VERBOSE=True):
             unzip_folder(localdir)
 
 
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print("get_data has 1 parameter: path to the folder, where the data will be downloaded")
-        print("----------python get_data.py your_path")
-    else:
-        path = sys.argv[1]
-        print("will save data into {}".format(path))
-        get_data(path)
+def main():
+    try:
+        options, args = getopt.getopt(sys.argv[1:], "vgrsh", ["verbose", "historical", "recent", "hourly","help"])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print(err)  # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    verbose = False
+    historical = False
+    recent = False
+    hourly = False
+    help = False
+    for opt, arg in options:
+        if opt in ("-v", "--verbose"): verbose = True
+        elif opt in ("-h", "--help"):
+            help = True
+            usage()
+        elif opt in ("-g","--historical"): historical = True
+        elif opt in ("-r", "--recent"): recent = True
+        elif opt in ("-s","--hourly"): hourly = True
+        else: assert False, "unhandled option"
+
+    try: path = args[0]
+    except IndexError:
+        if not help:
+            print("Must provide a path.")
+            usage()
+        sys.exit(2)
+
+    get_data(userpath = path,
+             historical = historical,
+             recent = recent,
+             hourly = hourly,
+             verbose = verbose)
+
+def usage():
+    print("""Usage: 'python get_data -vgrsh path'\n
+    arguments:\n
+    \t path : the path where you want to save the data \n
+    options:\n
+    \t -v/--verbose : verbose mode\n
+    \t -g/--historical : download historical data\n
+    \t -r/--recent : download recent data\n
+    \t -s/--hourly : download recent and historical hourly data
+    \t -h/--help
+    """)
+
+if __name__ == "__main__":
+    main()
