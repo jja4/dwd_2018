51111dcabb874291b6db1a945b2b04e06692b139
Bence Bagi <bagi.bence94@gmail.com>
2018-06-11 20:56:12 +0200

    Date conversion in clean_merged was wrong. TODO: only do this if we are working with daily data.

--- process_data.py
+++ process_data.py
@@ -83,7 +83,9 @@ def clean_merged(merged):
     merged_clean = merged.replace(-999, np.nan, regex=True)
     merged_clean = merged_clean.drop(['eor'],axis=1)
     merged_clean.columns = [c.strip().lower() for c in merged_clean.columns]
-    merged_clean['mess_datum'] = pd.to_datetime(merged_clean['mess_datum'].apply(str))
+    merged_clean['mess_datum'] = pd.to_datetime(merged_clean['mess_datum']
+    # if it is daily data
+    #merged_clean['mess_datum'] = merged_clean['mess_datum'].apply(str)).apply(lambda x: x.date())
     merged_clean = merged_clean.drop_duplicates(subset = ['stations_id', 'mess_datum'], keep='first')
 
     return merged_clean
